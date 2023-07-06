7e0c6a0e8525702a3e7dc793669ab9e3c71612b7
soualif <electlikon@gmail.com>
2023-07-06 19:40:37 +0200

    commit-repair-breadcrumb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,7 +54,6 @@
                             <img src="img/features/f-icon1.png" alt="">
                         </div>
                         <h6>Free Delivery</h6>
-                        <p>Free Shipping on all order</p>
                     </div>
                 </div>
                 <!-- single features -->
@@ -64,7 +63,6 @@
                             <img src="img/features/f-icon2.png" alt="">
                         </div>
                         <h6>Return Policy</h6>
-                        <p>Free Shipping on all order</p>
                     </div>
                 </div>
                 <!-- single features -->
@@ -74,7 +72,6 @@
                             <img src="img/features/f-icon3.png" alt="">
                         </div>
                         <h6>24/7 Support</h6>
-                        <p>Free Shipping on all order</p>
                     </div>
                 </div>
                 <!-- single features -->
@@ -84,7 +81,6 @@
                             <img src="img/features/f-icon4.png" alt="">
                         </div>
                         <h6>Secure Payment</h6>
-                        <p>Free Shipping on all order</p>
                     </div>
                 </div>
             </div>
@@ -101,11 +97,7 @@
                             <div class="col-lg-6 text-center">
                                 <div class="section-title">
                                       <h1>Latest Products</h1>
-                                      <p>
-                                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, vero autem. Nemo id minima
-                                          sint corrupti recusandae quibusdam dignissimos iste ipsam aut distinctio? Cum, mollitia. Quas
-                                          
-                                      </p>
+                                      <p></p>
                                 </div>
                             </div>
                         </div>
@@ -149,10 +141,7 @@
                     <div class="col-lg-6 text-center">
                         <div class="section-title">
                             <h1>Best Sellers</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                                labore et
-                                dolore
-                                magna aliqua.</p>
+                            <p></p>
                         </div>
                     </div>
                 </div>
@@ -191,27 +180,4 @@
     </section>
     <!-- end product Area -->
 
-    <!-- Start brand Area -->
-    <section class="brand-area section_gap">
-        <div class="container">
-            <div class="row">
-                <a class="col single-img" href="#">
-                    <img class="img-fluid d-block mx-auto" src="img/brand/1.png" alt="">
-                </a>
-                <a class="col single-img" href="#">
-                    <img class="img-fluid d-block mx-auto" src="img/brand/2.png" alt="">
-                </a>
-                <a class="col single-img" href="#">
-                    <img class="img-fluid d-block mx-auto" src="img/brand/3.png" alt="">
-                </a>
-                <a class="col single-img" href="#">
-                    <img class="img-fluid d-block mx-auto" src="img/brand/4.png" alt="">
-                </a>
-                <a class="col single-img" href="#">
-                    <img class="img-fluid d-block mx-auto" src="img/brand/5.png" alt="">
-                </a>
-            </div>
-        </div>
-    </section>
-    <!-- End brand Area -->
 @stop
